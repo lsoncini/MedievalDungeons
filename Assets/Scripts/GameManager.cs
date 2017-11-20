4cfd1f943bd75194f8e6a1a7c0354b52b0a94d86
lsoncini <lsoncini@itba.edu.ar>
2017-11-20 02:50:35 -0300

    added gameManager + PausePanel + PauseManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    public int timeForLevel = 0;
+
+    private bool isGameOn;
+    private int levelsWon = 0;
+    private int timeLeft;
+    private float t = 0;
+
+    [SerializeField] private GameObject gameLostPanel;
+    [SerializeField] private GameObject nextLevelPanel;
+
+    private void Awake() {
+        levelsWon = 0;
+        if (instance == null) {
+            instance = this;
+        } else if(instance != this) {
+            Destroy(gameObject);
+        }
+        NewLevel();
+    }
+
+    private void Update() {
+        t = Time.deltaTime;
+        if (t >= 1) {
+            TakeTime(1);
+        }
+        if(timeLeft <= 0) {
+            GameLost();
+        }
+    }
+
+    private void NewLevel() {
+        isGameOn = true;
+        timeLeft = timeForLevel;
+    }
+
+    public void AddTime(int timeToAdd) {
+        timeLeft += timeToAdd;
+    }
+
+    public void TakeTime(int timeToTake) {
+        if(timeLeft >= timeToTake) {
+            timeLeft -= timeToTake;
+        } else {
+            timeLeft = 0;
+        }
+    }
+
+    public bool IsGameOn() {
+        return isGameOn;
+    }
+
+    public void GameLost() {
+        gameLostPanel.SetActive(true);
+    }
+
+    public void LevelWon() {
+        levelsWon++;
+        nextLevelPanel.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PauseManager : MonoBehaviour {
+    [SerializeField] private GameObject pausePanel;
+
+    //private GameManager manager;
+
+    private void Awake() {
+        //manager = GameManager.instance;
+    }
+
+    void Update() {
+        //if (manager.IsGameOn()) {
+            if (Input.GetKeyDown(KeyCode.P)) {
+                if (!pausePanel.activeInHierarchy) {
+                    PauseGame();
+                } else {
+                    ContinueGame();
+                }
+            }
+        //}
+    }
+    public void PauseGame() {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+    }
+    public void ContinueGame() {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+}
