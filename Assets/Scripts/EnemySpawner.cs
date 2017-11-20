5ef5760bcefa6b426f4874f5031b1edbb17b5f18
lsoncini <lsoncini@itba.edu.ar>
2017-11-19 22:20:47 -0300

    added some more behaviour

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -8,8 +8,9 @@ public class EnemySpawner : MonoBehaviour {
     public GameObject enemy;
     public GameObject target;
     public float spawnTime = 1f;
+    public int maxEnemies = 5;
     private float t;
-    private int spiderCount = 0;
+    private int enemyCount = 0;
 
     private void Start() {
         t = spawnTime;
@@ -17,15 +18,15 @@ public class EnemySpawner : MonoBehaviour {
     private void OnTriggerStay2D(Collider2D collision) {
         t += Time.deltaTime;
         if(collision.tag == target.tag) {
-            if (t >= spawnTime) {
+            if (t >= spawnTime && enemyCount < maxEnemies) {
                 GameObject newGO = UnityEngine.Object.Instantiate(enemy) as GameObject;
-                newGO.name = String.Format("spider-{0}", spiderCount++);
+                newGO.name = String.Format("{0}-{1}", enemy.name, enemyCount++);
                 newGO.transform.parent = transform;
                 newGO.transform.localPosition = new Vector3(UnityEngine.Random.Range(-0.2f, 0.2f),
                                                             UnityEngine.Random.Range(-0.2f, 0.2f),
                                                             0);
                 newGO.GetComponent<TargetFollower>().target = target;
-                print(String.Format("spawning +{0}",spiderCount-1));
+                print(String.Format("spawning +{0}",enemyCount-1));
                 t = 0;
             }
         }
